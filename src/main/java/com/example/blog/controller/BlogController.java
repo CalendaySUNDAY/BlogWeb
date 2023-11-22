e9a47020570f1ff54ca51d3074f7baafe55d453a
calendar <calendarsunday@163.com>
2023-11-22 14:25:19 +0800

    Switch markdown rendering to frontbend.

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -1,9 +1,9 @@
 package com.example.blog.controller;
 
-import org.commonmark.node.*;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -21,47 +21,18 @@ public class BlogController {
         this.resourceLoader = resourceLoader;
     }
 
+
     @GetMapping("/{id}")
-    public String getBlogPost(@PathVariable String id) {
-        System.out.printf("Loading blog post %s\n", id);
+    public ResponseEntity<String> getBlogPost(@PathVariable String id) {
         try {
-            // Assume the Markdown file is in 'src/main/resources/blogs'
             String markdownPath = "classpath:/blogs/" + id + ".md";
+            // 直接读取文件内容
             String markdown = new String(Files.readAllBytes(Paths.get(resourceLoader.getResource(markdownPath).getURI())));
-
-            // Parse Markdown to HTML
-            Parser parser = Parser.builder().build();
-            Node document = parser.parse(markdown);
-            HtmlRenderer renderer = HtmlRenderer.builder().build();
-            String htmlContent = renderer.render(document);
-
-            // Your HTML template structure
-            String htmlTemplate = " <section class=\"u-align-center u-clearfix u-section-1\" id=\"sec-6f66\">\n" +
-                    "      <div class=\"u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1\"><!--post_details--><!--post_details_options_json--><!--{\"source\":\"\"}--><!--/post_details_options_json--><!--blog_post-->\n" +
-                    "        <div class=\"u-container-style u-expanded-width u-post-details u-post-details-1\">\n" +
-                    "          <div class=\"u-container-layout u-valign-middle u-container-layout-1\"><!--blog_post_image-->\n" +
-                    "            <img alt=\"\" class=\"u-blog-control u-expanded-width u-image u-image-default u-image-1\" src=\"../images/68f64b9d.jpeg\"><!--/blog_post_image--><!--blog_post_header-->\n" +
-                    "            <h2 class=\"u-blog-control u-text u-text-1\">Post 2 Headline</h2><!--/blog_post_header--><!--blog_post_metadata-->\n" +
-                    "            <div class=\"u-blog-control u-metadata u-metadata-1\"><!--blog_post_metadata_date-->\n" +
-                    "              <span class=\"u-meta-date u-meta-icon\">Nov 21, 2023</span><!--/blog_post_metadata_date--><!--blog_post_metadata_category-->\n" +
-                    "              <!--/blog_post_metadata_category--><!--blog_post_metadata_comments-->\n" +
-                    "              <!--/blog_post_metadata_comments-->\n" +
-                    "            </div><!--/blog_post_metadata--><!--blog_post_content-->\n" +
-                    "            <div class=\"u-align-justify u-blog-control u-post-content u-text u-text-2 fr-view\">\n" +
-                    "  Sample small text. Lorem ipsum dolor sit amet.\n" +
-                    "  </div><!--/blog_post_content-->\n" +
-                    "          </div>\n" +
-                    "        </div><!--/blog_post--><!--/post_details-->\n" +
-                    "      </div>\n" +
-                    "    </section>"; // Simplified for brevity
-
-            // Insert the HTML content into the template
-            String finalHtml = htmlTemplate.replace("Sample small text. Lorem ipsum dolor sit amet.", htmlContent);
-
-            return finalHtml;
+            return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).body(markdown);
         } catch (IOException e) {
             e.printStackTrace();
-            return "Error loading blog post";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error loading blog post");
         }
     }
+
 }
--- src/main/resources/static/blog/post.js
+++ src/main/resources/static/blog/post.js
@@ -12,34 +12,35 @@ function getQueryVariable(variable) {
     return null;
 }
 
-function fetchAndDisplayPostContent(postId) {
+function loadBlogContent(postId) {
     fetch(`/api/blogs/${postId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(html => {
-            const postContentDiv = document.getElementById('post-content');
-            if (postContentDiv) {
-                postContentDiv.innerHTML = html;
-            }
+        .then(response => response.text())
+        .then(markdown => {
+            // 使用marked.js将Markdown转换为HTML
+            const html = marked.parse(markdown);
+            console.log(html)
+            // 将HTML内容放到页面的某个元素中
+            document.getElementById('post-content').innerHTML = html;
+            // 如果您使用KaTeX，您可以在此处理数学公式
+            renderMathInElement(document.getElementById('post-content'), {
+                delimiters: [
+                    { left: "$$", right: "$$", display: true }, // 多行公式
+                    { left: "$", right: "$", display: false }, // 单行公式
+                ],
+            });
+
         })
         .catch(error => {
-            console.error('Error:', error);
-            const postContentDiv = document.getElementById('post-content');
-            if (postContentDiv) {
-                postContentDiv.innerHTML = `<p>Error loading blog post: ${error.message}</p>`;
-            }
+            console.error('Error loading blog content:', error);
         });
 }
+
 
 // 从URL获取postId
 const postId = getQueryVariable('post_id');
 
 if (postId) {
-    fetchAndDisplayPostContent(postId);
+    loadBlogContent(postId);
 } else {
     console.error('Post ID not found in URL');
 }
