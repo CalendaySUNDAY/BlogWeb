ca7f9f06917c0da9b87bcb16406a459960afacea
calendar <calendarsunday@163.com>
2023-11-24 14:50:56 +0800

    add markdown editor based on Editor.md

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -6,9 +6,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/blogs")
@@ -35,4 +42,84 @@ public class BlogController {
         }
     }
 
+    @GetMapping("/blog_general")
+    public ResponseEntity<String> getBlogNum() {
+        File folder = new File("src/main/resources/blogs/");
+        File[] listOfFiles = folder.listFiles();
+        if (listOfFiles != null) {
+            // 使用Lambda表达式创建一个Comparator来比较文件名
+            Arrays.sort(listOfFiles, (f1, f2) -> f1.getName().compareTo(f2.getName()));
+        }
+            List<Map<String, String>> filesData = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            if (listOfFiles != null) {
+                for (File file : listOfFiles) {
+                    if (file.isFile() && file.getName().endsWith(".md")){
+                        Map<String, String> fileData = new HashMap<>();
+                        fileData.put("name", file.getName());
+                        String content = readFileContent(file);
+                        fileData.put("head", content.length() > 20 ? content.substring(0, 20) : content);
+                        filesData.add(fileData);
+                    }
+                }
+            }
+            // 使用ObjectMapper将filesData转换为JSON字符串
+            String json = objectMapper.writeValueAsString(filesData);
+            return ResponseEntity.ok(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error loading blog posts");
+        }
+    }
+
+    @PostMapping("/uploadPicture")
+    public ResponseEntity<String> getPicture(@RequestParam("file") MultipartFile file) {
+        try {
+            // 检查文件是否为空
+            if (file.isEmpty()) {
+                return ResponseEntity.badRequest().body("File is empty");
+            }
+
+            // 获取文件MIME类型并进行检查
+            String mimeType = file.getContentType();
+            if(mimeType == null || (!mimeType.equals("image/jpeg") && !mimeType.equals("image/png"))) {
+                return ResponseEntity.badRequest().body("Invalid image file type");
+            }
+
+            // 生成文件名，这里使用了UUID来确保文件名的唯一性
+            String filename = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+
+            // 设置文件存储路径
+            Path path = Paths.get("blogs/pic_cache/" + filename);
+
+            // 确保目录存在
+            if (!Files.exists(path.getParent())) {
+                Files.createDirectories(path.getParent());
+            }
+
+            // 保存文件到服务器
+            Files.copy(file.getInputStream(), path);
+
+            // 返回pic_id（在这个场景中就是文件名）
+            return ResponseEntity.ok(filename);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Could not store the file");
+        }
+    }
+
+    private String readFileContent(File file) {
+        // 使用Java 8的Stream API来读取文件内容
+        try (Stream<String> stream = Files.lines(Paths.get(file.getPath()))) {
+            return stream.collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
+
+
--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -4,14 +4,73 @@ function loadBlogContent(postId) {
     window.open(`blog/post.html?post_id=${postId}`, '_blank');
 }
 
-// 绑定点击事件到所有具有'class="blog-post-link"'的链接
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.blog-post-link');
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
+    // 使用事件委托绑定事件到父元素,可以将监听绑定到未来创建的元素上
+    document.getElementById('blog-post-list-zgz').addEventListener('click', function (event) {
+        // 确定点击的是类名为 blog-post-link 的元素
+        if (event.target && event.target.matches('.blog-post-link')) {
             event.preventDefault(); // 阻止链接的默认行为
-            const postId = this.getAttribute('data-post-id');
+            const postId = event.target.getAttribute('data-post-id');
             loadBlogContent(postId);
-        });
+        }
     });
 });
+
+
+
+async function loadBlogDiv() {
+    try {
+        // 等待 fetch 请求完成
+        const response = await fetch('/api/blogs/blog_general');
+
+        // 检查 response 是否成功
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.json();
+        // 使用获取到的数据
+        data.forEach(post => {
+            const blogPostHTML = createBlogPostHTML(post);
+            document.getElementById('blog-post-list-zgz').innerHTML += blogPostHTML;
+        });
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+
+// Function to create HTML block for each post
+function createBlogPostHTML(post) {
+    // Extract the name and head for each post
+    const title = post.name.replace('.md', ''); // Removing the .md extension
+    const content = post.head.replace('›', ''); // Adjusting the head content
+
+    // Create the HTML block, using template literals
+    return `
+    <div class="u-blog-post u-container-style u-repeater-item">
+      <div class="u-container-layout u-similar-container u-valign-top u-container-layout-1">
+        <!--blog_post_header-->
+        <h2 class="u-blog-control u-text u-text-1">
+         ${title}
+        </h2><!--/blog_post_header-->
+        
+          <img alt="" class="u-blog-control u-expanded-width u-image u-image-default u-image-1"
+               src="images/8ad73f3c.jpeg"><!--/blog_post_image-->
+      
+        <div class="u-blog-control u-post-content u-text u-text-2 fr-view">${content}
+        </div><!--/blog_post_content--><!--blog_post_metadata-->
+        <div class="u-blog-control u-metadata u-metadata-1"><!--blog_post_metadata_date-->
+          <span class="u-meta-date u-meta-icon">Nov 21, 2023</span><!--/blog_post_metadata_date-->
+        </div><!--/blog_post_metadata--><!--blog_post_readmore-->
+        <a href="#" data-post-id="${title}"
+           class="blog-post-link u-active-none u-blog-control u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-base u-btn u-btn-rectangle u-button-style u-hover-none u-none u-btn-1">Read
+          More</a>
+      </div>
+    </div>`;
+}
+
+window.onload = function() {
+    loadBlogDiv();
+
+};
